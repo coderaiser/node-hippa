bdba29e0e052b7edd768b86db52482f9c4b1595d
coderaiser <coderaiser@cloudcmd.io>
2018-02-09 11:13:18 +0200

    feature(hippa) rm iife

--- bin/hippa.js
+++ bin/hippa.js
@@ -1,57 +1,55 @@
 #!/usr/bin/env node
 
-(function() {
-    'use strict';
+'use strict';
+
+var hippo       = require('..');
+var tryRequire = require('tryrequire');
+
+var HOME_WIN = process.env.HOMEPATH;
+var HOME_UNIX = process.env.HOME;
+var HOME = (HOME_UNIX || HOME_WIN) + '/';
+
+var name = HOME + '.hippa.json';
+
+var config = tryRequire(name) || {};
+
+var argv = process.argv.slice(2);
+var arg = argv[0];
+
+if (/-v|--version/.test(arg))
+    version();
+else if (!arg || /-h|--help/.test(arg))
+    help();
+else
+    main();
+
+function main() {
+    if (config.token)
+        hippo(config.token);
     
-    var hippo       = require('..'),
+    hippo.read(arg, function(error, data) {
+        if (error)
+            console.error(error.message);
+        else if (!Array.isArray(data))
+            console.log(data);
+        else
+            data.forEach(function(file) {
+                console.log(file.name);
+            });
+    });
+}
+
+function version() {
+    console.log('v' + info().version);
+}
+
+function info() {
+    return require('../package');
+}
+
+function help() {
+    var usage       = 'Usage: ' + info().name + ' <owner/repo/path>';
         
-        tryRequire  = require('tryrequire'),
-        
-        HOME_WIN    = process.env.HOMEPATH,
-        HOME_UNIX   = process.env.HOME,
-        HOME        = (HOME_UNIX || HOME_WIN) + '/',
-        
-        name        = HOME + '.hippa.json',
-        
-        config      = tryRequire(name) || {},
-        
-        argv        = process.argv.slice(2),
-        arg         = argv[0];
-    
-    if (/-v|--version/.test(arg))
-        version();
-    else if (!arg || /-h|--help/.test(arg))
-        help();
-    else
-        main();
-    
-    function main() {
-        if (config.token)
-            hippo(config.token);
-        
-        hippo.read(arg, function(error, data) {
-            if (error)
-                console.error(error.message);
-            else if (!Array.isArray(data))
-                console.log(data);
-            else
-                data.forEach(function(file) {
-                    console.log(file.name);
-                });
-        });
-    }
-    
-    function version() {
-        console.log('v' + info().version);
-    }
-    
-    function info() {
-        return require('../package');
-    }
-    
-    function help() {
-        var usage       = 'Usage: ' + info().name + ' <owner/repo/path>';
-            
-        console.log(usage);
-    }
-})();
+    console.log(usage);
+}
+
